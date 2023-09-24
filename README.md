de63beb141c0b77538343aada4d1ab5d89c1a3f9
Nomin Myagmarsuren <nominmyagmarsuren@gmail.com>
2023-09-24 01:41:41 -0600

    fixed shape and color not taking

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# SVG logo Creator
+# SVG Logo Creator
   
 ## Description 
   Using inquirer and jest, this application can create svg logos with the users input. The user will be asked questions about what text, text color, shape, and shape color they want. The answers will then be written in an SVG file. After having used inquirer already to create a readme file for a previous challenge, I already knew how to set it up. However a challenge was learning about SVG files and how to format it. After getting the coding in, I had to figure out how to get a preview of it to make sure it was displaying properly. This was another challenge that was just back-end with no front end, but using jest to test was 
   
-  ## Table of Contents
+## Table of Contents
   - [Installation](#installation)
   - [Usage](#usage)
   - [License](#license)
@@ -11,29 +11,29 @@
   - [Tests](#tests)
   - [Questions](#questions)
   
-  ## Link
+## Link
   (no deployed page) https://github.com/nomin97/svg-logo
   
-  ## Installation 
+## Installation 
   First clone the repo and then open in VS Code. Node must be installed, and install inquirer and 'jest' packages. Once package.json in installed, install jest. Once installed, user can run the app with command node index.js. 
   
-  ## Usage
+## Usage
   
   
-  ## Tests
-  Testing can be found in the 'lib' folder. 
+## Tests
+Testing can be found in the 'lib' folder. 
   
-  ## Credits
+## Credits
   Classwork and tutor Ashton. 
   
-  ## Questions?
+## Questions?
   * Email: <nominmyagmarsuren@yahoo.com>
   * GitHub: <https://github.com/nomin97>
   
-  ## Contribute
+## Contribute
   If you would like to contribute please reach out!
   
-  ## License
+## License
   This project is protected under the undefined.
   
   For more info, visit .
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'text-color',
+        name: 'textColor',
         message: 'Choose a color for the text.',
     },
     {
@@ -20,50 +20,44 @@ const questions = [
         name: 'shape',
         message: 'Choose a shape (select one)',
         choices: [
-            'circle',
             'triangle',
-            'square']
+            'square',
+            'circle']
     },
     {
         type: 'input',
-        name: 'shape-color',
+        name: 'shapeColor',
         message: 'Choose a color for the shape.',
     },
 ];
 
-//  
+// function to write to the file using the inquirer  
 function writeToFile(fileName, answers) {
-    // File starts as an empty string
+    // empty string later to have user input
     let svgString = "";
-    // Sets width and height of logo container
+    // Set width and height of logo container
     svgString =
         '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-    // <g> tag wraps <text> tag so that user font input layers on top of polygon -> not behind
     svgString += "<g>";
     // Takes user input for shape choice and inserts it into SVG file
     svgString += `${answers.shape}`;
-
     // Conditional check takes users input from choices array and then adds polygon properties and shape color to SVG string
     let shapeChoice;
-    if (answers.shape === "Triangle") {
+    if (answers.shape === "triangle") {
         shapeChoice = new Triangle();
-        svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
-    } else if (answers.shape === "Square") {
+        svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}"/>`;
+    } else if (answers.shape === "square") {
         shapeChoice = new Square();
-        svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+        svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeColor}"/>`;
     } else {
         shapeChoice = new Circle();
-        svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
-    }
+        svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeColor}"/>`;
+    };
 
-    // <text> tag gives rise to text alignment, text-content/text-color taken in from user prompt and gives default font size of "40"
-    svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
-    // Closing </g> tag
-    svgString += "</g>";
-    // Closing </svg> tag
-    svgString += "</svg>";
+    // <text> tag aligns the inputted text with the color chosen
+    svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`; svgString += "</g>"; svgString += "</svg>";
 
-    // Using file system module to generate svg file, takes in file name given in the promptUser function, the svg string, and a ternary operator which handles logging any errors, or a "Generated logo.svg" message to the console  
+    // writing the file to the system, catching errors, if no error will show "generated logo.svg"
     fs.writeFile(fileName, svgString, (err) => {
         err ? console.log(err) : console.log("Generated logo.svg");
     });
@@ -73,17 +67,15 @@ function writeToFile(fileName, answers) {
 function init() {
     inquirer.prompt(questions)
         .then((answers) => {
-            // Error handling for text prompt (user must enter 3 characters or less for logo to generate)
+            // insures text input will fit criteria or display error
             if (answers.text.length > 3) {
                 console.log("Enter 3 characters or less.");
                 promptUser();
             } else {
-                // Calling write file function to generate SVG file
                 writeToFile("logo.svg", answers);
             }         
         });
 }
-
 
 // Function call to initialize app
 init();
